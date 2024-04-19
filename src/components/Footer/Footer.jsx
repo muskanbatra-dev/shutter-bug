3c2d95a01998f68c4586fdd80963d6aaba39519f
muskanbatra-dev <95659002+muskanbatra-dev@users.noreply.github.com>
2024-04-19 13:58:27 +0530

    footter added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import './Footer.css'
-
+import React from "react";
+import "./Footer.css";
+import { FaGithub } from "react-icons/fa6";
+import { FiTwitter } from "react-icons/fi";
+import { FaLinkedin } from "react-icons/fa6";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="footer__items">
+        <p className="footer__text">Made with 💜 by Muskan Batra</p>
+        <div className="footer__links">
+          <p>
+            <FaGithub size={20} color="white" />
+          </p>
+          <p>
+            <FiTwitter size={20} color="white" />
+          </p>
+          <p>
+            <FaLinkedin size={20} color="white" />
+          </p>
+        </div>
+        <p className="footer__copyright">@ShutterBug</p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
