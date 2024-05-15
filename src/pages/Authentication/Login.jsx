6ecc244d92ff5094c3ff24d59b6e3b986bfce878
muskanbatra-dev <95659002+muskanbatra-dev@users.noreply.github.com>
2024-05-16 02:07:31 +0530

    login

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -1,47 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import "./Auth.css"
-import FormInputText from '../../components/FormInputText/FormInputText'
+import "./Auth.css";
+import FormInputText from "../../components/FormInputText/FormInputText";
 import Navigation from "../../components/Navigation/Navigation";
 import Footer from "../../components/Footer/Footer";
-import axios from "axios"
-import {SET_TOKEN , SET_LOGIN, SET_USER} from "../../Redux/reducers/auth"
-import { useDispatch } from 'react-redux';
+import axios from "axios";
+import { SET_TOKEN, SET_LOGIN, SET_USER } from "../../Redux/reducers/auth";
+import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 const EMAIL_REGEX = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
 
 const Login = () => {
-  const [email,setEmail]= useState("")
-  const [password,setPassword]= useState("")
-  
-  const dispatch = useDispatch()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
   const testLogin = () => {
     try {
-      
       axios
-      .post(
-        "/api/auth/login",
-        {  email: "muskanBatr123@gmail.com",
-    password: "muskanbatra"},
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      )
-      .then((response) => {
-        console.log(response)
-        dispatch(SET_TOKEN(response.data.encodedToken))
-        dispatch(SET_LOGIN(true))
-        dispatch(SET_USER(response.data.foundUser))
-    toast.success(`Welcome,Guest User`);
-      });
+        .post(
+          "/api/auth/login",
+          { email: "muskanBatr123@gmail.com", password: "muskanbatra" },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          dispatch(SET_TOKEN(response.data.encodedToken));
+          dispatch(SET_LOGIN(true));
+          dispatch(SET_USER(response.data.foundUser));
+          toast.success(`Welcome,Guest User`);
+        });
     } catch (error) {
-      console.log(error)
-      toast.error(error)
+      console.log(error);
+      toast.error(error);
     }
-    
-  }
+  };
   const loginHandler = async () => {
     try {
       if (!EMAIL_REGEX.test(email)) {
@@ -53,84 +50,91 @@ const Login = () => {
         return;
       }
       axios
-      .post(
-        "/api/auth/login",
-        {
-          email,
-          password,
-          
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
+        .post(
+          "/api/auth/login",
+          {
+            email,
+            password,
           },
-        }
-      )
-      .then((response) => {
-        console.log(response)
-        dispatch(SET_TOKEN(response.data.encodedToken))
-        dispatch(SET_LOGIN(true))
-        dispatch(SET_USER(response.data.foundUser))
-      });
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          dispatch(SET_TOKEN(response.data.encodedToken));
+          dispatch(SET_LOGIN(true));
+          dispatch(SET_USER(response.data.foundUser));
+        });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-    
   };
   return (
     <div>
-      <Navigation/>
-    <main className='login-form'>
-    <form>
-      <p className='form-heading'>Login</p>
+      <Navigation />
+      <main className="login-form">
+        <form>
+          <p className="form-heading">Login</p>
 
-      <FormInputText
-        label={"Email"}
-        placeholder={"Johndoe@gmail.com"}
-        type={"email"}
-        value={email}
-        onChange={(e)=>{
-          setEmail(e.target.value.trim())
-        }}
-      />
-       <FormInputText
-        label={"password"}
-        placeholder={"*******"}
-        type={"password"}
-        value={password}
-        onChange={(e)=>{
-          setPassword(e.target.value.trim())
-        }}
-      />
+          <FormInputText
+            label={"Email"}
+            placeholder={"Johndoe@gmail.com"}
+            type={"email"}
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value.trim());
+            }}
+          />
+          <FormInputText
+            label={"password"}
+            placeholder={"*******"}
+            type={"password"}
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value.trim());
+            }}
+          />
 
-      <div className='forgotpwd__div'>
-        <label htmlFor="">
-        <input type="checkbox" /> Remember me
-        </label>
-      
-        <p>Forgot Password?</p>
-      </div>
-      <div className='btn'>
-        <NavLink >
-          <button className="primary_btn" onClick={()=>{
-            loginHandler()
-          }}>Login</button>
-        </NavLink>
-        <NavLink >
-          <button  onClick={()=>{
-            testLogin()
-          }}className="secondary_btn">Guest Login</button>
-        </NavLink>
-        </div>
-        <NavLink to="/sign-up" className="create__new__btn">
-        <p>Create New Account ❯</p>
-        </NavLink>
-       
-    </form>
-  </main>
-  <Footer/>
-  </div>
-  )
-}
+          <div className="forgotpwd__div">
+            <label htmlFor="">
+              <input type="checkbox" /> Remember me
+            </label>
 
-export default Login
+            <p>Forgot Password?</p>
+          </div>
+          <div className="btn">
+            <NavLink>
+              <button
+                className="primary_btn"
+                onClick={() => {
+                  loginHandler();
+                }}
+              >
+                Login
+              </button>
+            </NavLink>
+            <NavLink>
+              <button
+                onClick={() => {
+                  testLogin();
+                }}
+                className="secondary_btn"
+              >
+                Guest Login
+              </button>
+            </NavLink>
+          </div>
+          <NavLink to="/sign-up" className="create__new__btn">
+            <p>Create New Account ❯</p>
+          </NavLink>
+        </form>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Login;
